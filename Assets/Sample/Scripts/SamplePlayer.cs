eb4d189647ec2e2607d25dd58e9052cffc407462
Dan Westaway <dan.westaway@students.plymouth.ac.uk>
2018-01-09 19:45:58 +0000

    Added server
    Linked game to server, players are spawned through console commands, random spawn locations, changed movement

---
+++ Assets/Sample/Scripts/SamplePlayer.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using AlphaOwl.UniversalController;
+using AlphaOwl.UniversalController.Utilities;
+
+public class SamplePlayer : UCPlayer
+{
+    private Vector3 spawnPoint;
+	public float movementSpeed;
+	public float jump;
+	private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+
+		rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+	// Override callbacks from parent. Call the base methods 
+	// for debug usage.
+	protected override void OnPlayerRegisterAction()
+	{
+        base.OnPlayerRegisterAction();
+
+		float x = Random.Range (-10.0f, 10.0f);
+		float z = Random.Range (-10.0f, 10.0f);
+
+		DebugUtilities.Log(x + " hello " + z);
+
+		spawnPoint = new Vector3 (x, 5, z);
+
+        // Sample action
+        transform.position = spawnPoint;
+        Debug.Log("Player Spawned", this);
+    }
+
+	public override void OnPlayerDeregister()
+	{
+		// Remove this line if you don't want to destroy 
+		// the player gameobject when player deregister.
+        base.OnPlayerDeregister(); 
+    }
+
+    public override void KeyDown(string key, string extra = "")
+    {
+        base.KeyDown(key, extra);
+
+		switch(key)
+		{
+		case "jump":
+			rb.AddForce (Vector3.up * jump, ForceMode.Impulse);
+
+			break;
+		case "left":
+
+			transform.position += Vector3.left * Time.deltaTime * movementSpeed;
+
+			break;
+		case "right":
+
+			transform.position += Vector3.right * Time.deltaTime * movementSpeed;
+
+			break;
+		case "forwards":
+
+			transform.position += transform.forward * Time.deltaTime * movementSpeed;
+
+			break;
+		case "backwards":
+
+			transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+
+			break;
+		}
+
+        // Switch statement is recommended here to 
+        // response on different key inputs.
+    }
+
+    public override void Gyro(float x, float y, float z)
+	{
+		// Values of x, y, z are between 1 and -1.
+
+        base.Gyro(x, y, z);
+    }
+
+	public override void Joystick(float x, float y)
+	{
+		// Values of x, y are between 1 and -1.
+
+        base.Joystick(x, y);
+    }
+}
---
+++ Assets/UniversalController/Utilities/DebugUtilities.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace AlphaOwl.UniversalController.Utilities
+{
+
+    /// <summary>
+    /// Type of the log message.
+    /// </summary>
+    public enum LogType
+    {
+        Normal,
+        Warning,
+        Error
+    }
+
+    /// <summary>
+    /// Customised utilities for debug usage across the library.
+    /// </summary>
+    public static class DebugUtilities
+    {
+        public static bool Enable;
+
+        /// <summary>
+        /// Write log message to console.
+        /// </summary>
+        /// <param name="msg">Log message.</param>
+        /// <param name="obj">Object to which the message applies.</param>
+        /// <param name="type">Type of the log. 
+        /// Default == LogType.Normal</param>
+        public static void Log(string msg, Object obj = null, 
+        LogType type = LogType.Normal)
+        {
+            if (!Enable)
+                return;
+
+            switch (type)
+            {
+                case LogType.Normal:
+                    if (obj != null)
+                        Debug.Log(msg);
+                    else
+                        Debug.Log(msg, obj);
+                    break;
+                case LogType.Warning:
+                    if (obj != null)
+                        Debug.LogWarning(msg);
+                    else
+                        Debug.LogWarning(msg, obj);
+                    break;
+                case LogType.Error:
+                    if (obj != null)
+                        Debug.LogError(msg);
+                    else
+                        Debug.LogError(msg, obj);
+                    break;
+            }
+        }
+    }
+}
